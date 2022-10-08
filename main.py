89d8bc92bec65f45488a6b089dac3bb41b054a8a
Zachary Juang <zachary822@me.com>
2022-10-08 04:23:55 -0400

    Add logging

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import json
+import logging.config
+from pathlib import Path
+
+with (Path(__file__).resolve().parent / "logging.conf").open("r") as f:
+    logging.config.fileConfig(f)
+
+logger = logging.getLogger(__name__)
+
+
 def handler(event, context):
-    print(event)
+    if logger.isEnabledFor(logging.INFO):
+        logger.info("Event: %s", json.dumps(event))
 
     return {
         "statusCode": 200,
